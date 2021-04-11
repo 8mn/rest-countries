ea9844faae4a98c8bc12970170d4f1a58e60748a
calmighted <calmighted@gmail.com>
2021-04-11 12:36:32 +0530

    initial commit with example layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={styles.Header}>
+        <span className={styles.Heading}> Where in the world? </span>
+        <div>
+          Dark Mode
+        </div>
       </header>
+      <div className={styles.filterbox}>
+        <input type="text" placeholder="Search for a country..."/>
+        <div>
+          <div className={styles.filter}>
+          <label for="countries">Filter by Region</label>
+          {/* <select name="Countries" >
+            <option value="Africa">Africa</option>
+            <option value="America">America</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Oceania</option>
+          </select> */}
+          </div>
+        </div>
+      </div>
+      <div className={styles.countryContainer}>
+        <div className={styles.country}>
+            <img src="" alt="Country flag"/>
+            <div className={styles.countryInfo}>
+            <span>Country name</span>
+              <ul>
+              <li>
+              Population: 81,770900
+              </li>
+              <li>
+              Region: Europe
+              </li>
+              <li>
+              Capital: Berlin
+              </li>
+              </ul>
+            </div>
+        </div>
+        <div className={styles.country}>
+            <img src="" alt="Country flag"/>
+            <div className={styles.countryInfo}>
+            <span>Country name</span>
+              <ul>
+              <li>
+              Population: 81,770900
+              </li>
+              <li>
+              Region: Europe
+              </li>
+              <li>
+              Capital: Berlin
+              </li>
+              </ul>
+            </div>
+        </div>
+        <div className={styles.country}>
+            <img src="" alt="Country flag"/>
+            <div className={styles.countryInfo}>
+            <span>Country name</span>
+              <ul>
+              <li>
+              Population: 81,770900
+              </li>
+              <li>
+              Region: Europe
+              </li>
+              <li>
+              Capital: Berlin
+              </li>
+              </ul>
+            </div>
+        </div>
+        <div className={styles.country}>
+            <img src="" alt="Country flag"/>
+            <div className={styles.countryInfo}>
+            <span>Country name</span>
+              <ul>
+              <li>
+              Population: 81,770900
+              </li>
+              <li>
+              Region: Europe
+              </li>
+              <li>
+              Capital: Berlin
+              </li>
+              </ul>
+            </div>
+        </div>
+        <div className={styles.country}>
+            <img src="" alt="Country flag"/>
+            <div className={styles.countryInfo}>
+            <span>Country name</span>
+              <ul>
+              <li>
+              Population: 81,770900
+              </li>
+              <li>
+              Region: Europe
+              </li>
+              <li>
+              Capital: Berlin
+              </li>
+              </ul>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
