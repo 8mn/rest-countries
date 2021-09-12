4e9667d16e190ab09a92862c665cc2523719bfd7
calmighted <calmighted@gmail.com>
2021-09-12 17:42:52 +0530

    removed all unused vars

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,32 +1,14 @@
-import React,{useState} from 'react'
+import React from 'react'
 import classes from './Header.module.css'
 
 
 
 function Header() {
 
-    const[mode,setmode] = useState('Dark Mode')
-
-    const toggleMode = () => {
-        if (mode === 'Light Mode'){
-          setmode('Dark Mode')
-          seticon('Light')
-        }else{
-          setmode('Light Mode')
-          seticon('Dark')
-        }
-      }
-      
-
-
     return (
         <header className={classes.Header}>
         <div className={classes.headerContent}>
         <span className={classes.Heading}> Where in the world? </span>
-        {/* <div className={classes.toggleDark} onClick={toggleMode}>
-          {icon === "Dark"? <BiSun />: <BiMoon />}
-          {mode}
-        </div> */}
         </div>
       </header>
     )
